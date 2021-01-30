c2f451ebb71aa13e4657a377a8e88ea2adf36b6f
base0 <vacapp@live.com>
2021-01-30 11:18:11 +0700

    Create aquarium.py

---
+++ aquarium.py
@@ -0,0 +1,24 @@
+num1 = 10
+blocks.fill(GLASS,
+    world(0, 0, 0),
+    world(num1, num1, num1),
+    FillOperation.REPLACE)
+blocks.fill(AIR,
+    world(1, 1, 1),
+    world(num1 - 1, num1, num1 - 1),
+    FillOperation.REPLACE)
+blocks.fill(SOUL_SAND,
+    world(1, 1, 1),
+    world(num1 - 1, 1, num1 - 1),
+    FillOperation.REPLACE)
+blocks.fill(BLOCK_OF_QUARTZ,
+    world(2, 1, 2),
+    world(num1 - 2, 1, num1 - 2),
+    FillOperation.REPLACE)
+blocks.fill(WATER,
+    world(1, 2, 1),
+    world(num1 - 1, num1, num1 - 1),
+    FillOperation.REPLACE)
+for i in range(20):
+    mobs.spawn(TROPICAL_FISH, world(3, 3, 3))
+    
