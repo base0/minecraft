2697acf72b697a60d1260a2b5a481cf13c618d76
base0 <vacapp@live.com>
2021-02-03 14:54:02 +0700

    Update underwater_aquarium.py

--- underwater_aquarium.py
+++ underwater_aquarium.py
@@ -15,11 +15,15 @@ def water(w):
     y = 4
     blocks.fill(WATER, world(-w+1, w+y, -w+1), world(w-1, w+4, w-1), FillOperation.REPLACE)
 
-def rect(t, l, r, b, y):
-    shapes.line(SOUL_SAND, world(l, y, t), world(r, y, t))
-    shapes.line(SOUL_SAND, world(r, y, t), world(r, y, b))
-    shapes.line(SOUL_SAND, world(r, y, b), world(l, y, b))
-    shapes.line(SOUL_SAND, world(l, y, b), world(l, y, t))
+def rect(t, l, r, b, y, material):
+    if material == 1:
+        material = SEA_LANTERN
+    else:
+        material = SOUL_SAND
+    shapes.line(material, world(l, y, t), world(r, y, t))
+    shapes.line(material, world(r, y, t), world(r, y, b))
+    shapes.line(material, world(r, y, b), world(l, y, b))
+    shapes.line(material, world(l, y, b), world(l, y, t))
 
 def summon(number):
     for i in range(number):
@@ -28,16 +32,16 @@ def summon(number):
         #mobs.spawn(PUFFERFISH, pos(0, 30, 0))
         mobs.spawn(TROPICAL_FISH, pos(0, 9, 0))
 
-def light(w, y):
+def rectangle(w, y, m):
     y += 4
-    rect(-w, -w, w, w, y)
+    rect(-w, -w, w, w, y, m)
     
 def clear():
     blocks.fill(AIR, world(-20, 4, -20), world(20, 20, 20), FillOperation.REPLACE)
 
 player.on_chat("c", clear)
 player.on_chat("b", building)
-player.on_chat('l',  light)
+player.on_chat('r',  rectangle)
 player.on_chat('w',  water)
 player.on_chat('s', summon)
 
