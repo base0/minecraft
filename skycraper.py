4fa89ea1822b1e522e7033d9a7a3dc4da9952d01
base0 <vacapp@live.com>
2021-02-10 06:50:22 +0700

    Create skycraper.py

---
+++ skycraper.py
@@ -0,0 +1,53 @@
+player_position = player.position()
+player.say(player_position)
+player.teleport(pos(-20, 0, 10))
+flr = [
+    '100000000000002001',
+    '100000000000020001',
+    '100000000000200001',
+    '111000011111111111',
+]
+
+
+t = []
+for i in range(len(flr)):
+    t.append(flr[len(flr) - i - 1])
+flr = t
+
+even_flr = []
+for i in range(len(flr)):
+    s = ''
+    for j in range(len(flr[i])):
+        tmp = flr[i]
+        s += tmp[len(tmp) - 1 - j]
+    even_flr.append(s)
+
+
+DEPTH = 5
+for FL in range(60):
+    for x in range(DEPTH):
+        for y in range(len(flr)):
+            for z in range(len(flr[0])):
+                b = AIR
+                if x == 0:
+                    b = BRICKS
+                elif x < 3:
+                    if FL % 2 == 0:
+                        if flr[y][z] != '0':
+                            b = BRICKS
+                    else:
+                        if even_flr[y][z] != '0':
+                            b = BRICKS
+                else:
+                    if y == 0:
+                        b = BRICKS
+                    else:
+                        if FL % 2 == 0:
+                            if flr[y][z] == '1':
+                                b = BRICKS
+                        else:
+                            if even_flr[y][z] == '1':
+                                b = BRICKS
+
+                blocks.place(b, positions.add(player_position, world(-x, FL * len(flr) + y, z)))
+
