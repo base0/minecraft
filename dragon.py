e485bbfca27b582582f7b746ba472c3f9438d688
base0 <vacapp@live.com>
2021-02-11 11:26:11 +0700

    Update dragon.py

--- dragon.py
+++ dragon.py
@@ -1,21 +1,22 @@
 xs = [1.0,
- 0.7071067811865476,
- 6.123233995736766e-17,
- -0.7071067811865475,
- -1.0,
- -0.7071067811865477,
- -1.8369701987210297e-16,
- 0.7071067811865474,
- 1.0]
+    0.7071067811865476,
+    6.123233995736766e-17,
+    -0.7071067811865475,
+    -1.0,
+    -0.7071067811865477,
+    -1.8369701987210297e-16,
+    0.7071067811865474,
+    1.0]
+
 zs = [0.0,
- 0.7071067811865475,
- 1.0,
- 0.7071067811865476,
- 1.2246467991473532e-16,
- -0.7071067811865475,
- -1.0,
- -0.7071067811865477,
- -2.4492935982947064e-16]
+    0.7071067811865475,
+    1.0,
+    0.7071067811865476,
+    1.2246467991473532e-16,
+    -0.7071067811865475,
+    -1.0,
+    -0.7071067811865477,
+    -2.4492935982947064e-16]
 
     
 spiral_radius = 6
@@ -26,15 +27,15 @@ player_position = player.position()
 player.say(player_position)
 
 def offset(x, y, z):
-  return positions.add(player_position, pos(x, y, z))
+    return positions.add(player_position, pos(x, y, z))
 
-shapes.line(RED_CONCRETE, offset(0, 0, 0), offset(0, 100, 0))
+shapes.line(RED_CONCRETE, offset(0, 0, 0), offset(0, 108, 0))
 
 y = radius / 2
-for i in range(len(xs)):
-    b = GRASS if i < 8 else GOLD_BLOCK
-    x = spiral_radius * xs[i]
-    z = spiral_radius * zs[i]
+for i in range(4*len(xs)):
+    b = LIME_GLAZED_TERRACOTTA #GRASS if i < 8 else GOLD_BLOCK
+    x = spiral_radius * xs[i%9]
+    z = spiral_radius * zs[i%9]
     player.say(x+' ' + y + ' '  + z)
     shapes.sphere(b, offset(x, y, z), radius, ShapeOperation.OUTLINE)
     y += dy
@@ -43,11 +44,11 @@ for i in range(len(xs)):
 radius -= 1
 y -= dy
 d = radius + 2
-blocks.fill(GRASS, offset(x - radius, y - d, z), offset(x + radius, y + d, z + radius * 2))
+blocks.fill(LIME_GLAZED_TERRACOTTA, offset(x - radius, y - d, z), offset(x + radius, y + d, z + radius * 2))
 
 
 # mouth
-blocks.fill(GRASS, offset(x - radius, y - d, z + radius * 2), offset(x + radius, y, z + radius * 5))
+blocks.fill(YELLOW_GLAZED_TERRACOTTA, offset(x - radius, y - d, z + radius * 2), offset(x + radius, y, z + radius * 5))
 
 # eye
 blocks.fill(GLASS, offset(x + radius - 1, y + d - 2, z + radius * 2 - 1),
