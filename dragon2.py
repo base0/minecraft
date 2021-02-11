ccbad0af70358ec529a9a95f74d47c62e68553a9
base0 <vacapp@live.com>
2021-02-11 14:24:30 +0700

    Create dragon2.py

---
+++ dragon2.py
@@ -0,0 +1,68 @@
+xs = [1.0,
+    0.7071067811865476,
+    6.123233995736766e-17,
+    -0.7071067811865475,
+    -1.0,
+    -0.7071067811865477,
+    -1.8369701987210297e-16,
+    0.7071067811865474,
+    1.0]
+
+zs = [0.0,
+    0.7071067811865475,
+    1.0,
+    0.7071067811865476,
+    1.2246467991473532e-16,
+    -0.7071067811865475,
+    -1.0,
+    -0.7071067811865477,
+    -2.4492935982947064e-16]
+
+    
+spiral_radius = 6
+radius = 3
+dy = radius
+
+player_position = player.position()
+player.teleport(offset(-26, 116, 44))
+
+def offset(x, y, z):
+    return positions.add(player_position, pos(x, y, z))
+
+ROUND = 4
+shapes.line(RED_CONCRETE, offset(0, 0, 0), offset(0, ROUND * 9 * radius, 0))
+
+y = radius / 2
+for i in range(ROUND*len(xs)):
+    b = LIME_GLAZED_TERRACOTTA #GRASS if i < 8 else GOLD_BLOCK
+    x = spiral_radius * xs[i%9]
+    z = spiral_radius * zs[i%9]
+    player.say(x+' ' + y + ' '  + z)
+    shapes.sphere(b, offset(x, y, z), radius, ShapeOperation.OUTLINE)
+    y += dy
+
+# z back->front
+# x right->left
+
+# head
+radius -= 1
+y -= dy
+d = radius + 2
+blocks.fill(LIME_GLAZED_TERRACOTTA, offset(x - radius, y - d, z), offset(x + radius, y + d-1, z + radius * 2 + 2))
+
+
+# mouth
+blocks.fill(YELLOW_GLAZED_TERRACOTTA, offset(x - radius, y - d, z + radius * 2 - 2), offset(x + radius, y, z + radius * 5))
+
+blocks.fill(AIR, offset(x - radius, y - d + 1, z + radius * 2 + 2), offset(x + radius, y - d +1, z + radius * 5))
+
+# eye
+blocks.fill(GLASS, offset(x + radius - 1, y + d - 3, z + radius * 2 - 1),
+                   offset(x + radius,     y + d - 2, z + radius * 2))
+
+blocks.fill(GLASS, offset(x - radius + 1, y + d - 3, z + radius * 2 - 1),
+                   offset(x - radius,     y + d - 2, z + radius * 2))
+
+# horn
+blocks.fill(RED_GLAZED_TERRACOTTA, offset(x - radius, y + d - 2, z - 5), offset(x - radius, y + d, z + 1))               
+blocks.fill(RED_GLAZED_TERRACOTTA, offset(x + radius, y + d - 2, z - 5), offset(x + radius, y + d, z + 1))               
