4eac6a6e5072945320d2422d0f8eeebab09086be
base0 <vacapp@live.com>
2021-02-13 22:06:52 +0700

    Create scale_pond2.py

---
+++ scale_pond2.py
@@ -0,0 +1,34 @@
+player_position = player.position()
+#player.teleport(rpos(17, 32, -1))  # 3
+player.teleport(rpos(35, 46, -3))  # 5
+
+def rpos(x, y, z):
+    return positions.add(player_position, pos(x, y, z))
+
+RADIUS = 5
+MAX = 3
+
+x = 0
+z = 0
+for i in range(MAX):
+    if i > MAX // 2 - 1:
+        for j in range(i+1):
+            for y in range(2):
+                shapes.circle(STONE_BRICKS, rpos(x, y, z - RADIUS * 2 * j), RADIUS, Axis.Y, ShapeOperation.HOLLOW)
+            blocks.fill(AIR, rpos(x + 1, 0, z - RADIUS * 2 * j + RADIUS), rpos(x + RADIUS, 1, z - RADIUS * 2 * j - RADIUS), FillOperation.REPLACE)
+    x += 0.87*RADIUS*2
+    z += 0.5*RADIUS*2
+
+x = 0.87 * RADIUS * 2 * (MAX-1)
+z = 0
+for y in range(2):
+    shapes.circle(STONE_BRICKS, rpos(x, y, z), RADIUS * (MAX+1), Axis.Y, ShapeOperation.OUTLINE)
+
+d = RADIUS * (MAX+1)-1
+for rx in range(-d, d):
+    for rz in range(-d, d):
+        if ((rx)**2+(rz)**2)**0.5 <  d:
+            if blocks.test_for_block(STONE_BRICKS, rpos(x + rx, 1, z + rz)) == False:
+                blocks.place(WATER, rpos(x + rx, 1, z + rz))
+                if randint(0, 3) == 0:
+                    mobs.spawn(TROPICAL_FISH, rpos(x + rx, 1, z + rz))
