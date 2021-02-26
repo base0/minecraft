5233e7a714b9c63746e106c982e1659a1ba46a79
base0 <vacapp@live.com>
2021-02-26 12:25:53 +0700

    Update 0.py

--- 0.py
+++ 0.py
@@ -23,8 +23,16 @@ def rect(b, x1, z1, x2, z2, y):
     blocks.fill(b, rpos(x1,y,z1), rpos(x2,y,z2))
     blocks.fill(AIR, rpos(x1+1,y,z1+1), rpos(x2-1,y,z2-1))
 
-def octagon(b, x1, z1, x2, z2, y, c):
+def octagon_outline(b, x1, z1, x2, z2, y, c):
     a = [rpos(x1+c,y,z1), rpos(x2-c,y,z1), rpos(x2,y,z1+c), rpos(x2,y,z2-c), rpos(x2-c,y,z2),rpos(x1+c,y,z2),rpos(x1,y,z2-c),rpos(x1,y,z1+c)]
     a.append(a[0])
     for i in range(len(a)-1):
         shapes.line(b, a[i], a[i+1])
+        
+def octagon_fill(b, x1, z1, x2, z2, y, c):
+    blocks.fill(b, pos(x1, y, z1), pos(x2,y,z2))
+    for i in range(c):
+        shapes.line(AIR, rpos(x1, y, z1+i), rpos(x1+i, y, z1))
+        shapes.line(AIR, rpos(x2, y, z1+i), rpos(x2-i, y, z1))
+        shapes.line(AIR, rpos(x2, y, z2-i), rpos(x2-i, y, z2))
+        shapes.line(AIR, rpos(x1, y, z2-i), rpos(x1+i, y, z2))
